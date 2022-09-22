ca62bfdd5ba83e72d3930f9b4e8dfd2f56fe7d41
ligadobra <russiancmo@gmail.com>
2022-09-22 16:42:11 +0300

    fix login data

--- packages/frontend/src/pages/Login/Login.tsx
+++ packages/frontend/src/pages/Login/Login.tsx
@@ -78,7 +78,7 @@ export const Login = () => {
                 initialValues={{ remember: true }}
                 onFinishFailed={onFinishFailed}
             >
-                <Form.Item name="login" label="Логин1">
+                <Form.Item name="login" label="Логин">
                     <Input name="login" />
                 </Form.Item>
                 <Form.Item name="password" label="Пароль">
@@ -86,7 +86,7 @@ export const Login = () => {
                 </Form.Item>
                 <Row className="button__group">
                     <Col>
-                        <SubmitButton shape="round">Войти1</SubmitButton>
+                        <SubmitButton shape="round">Войти</SubmitButton>
                     </Col>
                     <Col>
                         <Button type="link" onClick={() => navigate(ProjectRoutes.signUp)}>
